c78735043a4c7922ccf3234aa6e045d8c687058e
NithikornJ <nithikorn.jst@gmail.com>
2025-07-13 00:09:15 +0700

    add Loading

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,5 +1,8 @@
 import { fetchProduct } from "@/lib/api";
 import ProductCard from "@/components/ProductCard";
+import Loading from "@/components/Loading";
+import { Suspense } from "react";
+
 
 
 
@@ -15,11 +18,13 @@ export default async function ProductsPage() {
     <>
     <div className="max-w-6xl mx-auto px-4 py-10">
       <h1 className="text-3xl font-bold mb-6">สินค้าทั้งหมด</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      <Suspense fallback={<Loading />}>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </Suspense>
     </div>
     </>
   )
